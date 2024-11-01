24e47b4fbda3439e5d2e1fcd739018dc3b1eba45
Tony Wu <wu.anthon@northeastern.edu>
2024-11-01 18:35:42 -0400

    tests(dataProcessPlotsPTM): Fix unit tests in dataProcessPlotsPTM

--- tests/testthat/test_dataProcessPlotsPTM.R
+++ tests/testthat/test_dataProcessPlotsPTM.R
@@ -1,15 +1,15 @@
+library(grid)
+
 test_that("Medians are equalized in ptm data to QC plot helper function", {
-    mock_qc_all_plot_lf <- mock()
+    mock_qc_all_plot_lf <- mock(rectGrob(), cycle = TRUE)
     stub(
         dataProcessPlotsPTM, ".qc.all.plot.lf",
         mock_qc_all_plot_lf, depth=2
     )
-    
     dataProcessPlotsPTM(summary.data,
                         type = 'QCPLOT',
                         which.PTM = "allonly",
                         address = FALSE)
-    
     calls <- mock_args(mock_qc_all_plot_lf)
     ptm_data <- calls[[1]][[1]]
     run_data = split(ptm_data, ptm_data$RUN)
